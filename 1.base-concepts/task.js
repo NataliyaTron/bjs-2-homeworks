7e020d8d99dfe60d4fdabe792eb1d57d7edbfd75
NataliyaTron <118108299+NataliyaTron@users.noreply.github.com>
2023-02-11 12:43:42 +0400

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,21 @@
 "use strict"
 function solveEquation(a, b, c) {
+  
   let arr = [];
+  let x1;
+  let x2;
   
-  return arr;
-}
+  let D = Math.pow(b, 2) - 4 * a * c;
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (D < 0) {
+    arr = [];
+  } else if (D === 0) {
+    arr = [-b/(2*a)];
+  } else {
+    x1 = (-b + Math.sqrt(D)) / (2 * a);
+    x2 = (-b - Math.sqrt(D)) / (2 * a);
+    arr.push(x1, x2);
+  }
+
+    return arr;
 }
