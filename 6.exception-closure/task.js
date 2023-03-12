68aab48b60271bb26cb0b72a6217ab355022eee9
NataliyaTron <118108299+NataliyaTron@users.noreply.github.com>
2023-03-12 12:16:26 +0400

    Шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,55 @@
-﻿
+﻿// Задача 1. Форматтер чисел
+
+function parseCount(numberOfPurchases) {
+  if (Number.isNaN(Number.parseFloat(numberOfPurchases))) {
+    throw new Error ('Невалидное значение');
+  }
+  return Number.parseFloat(numberOfPurchases);
+}
+
+function validateCount(numberOfPurchases){
+  try {
+    return parseCount(numberOfPurchases);
+  } catch (error) {
+    return error;
+  }
+}
+
+
+// Задача 2. Треугольник
+
+class Triangle {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    if (a + b < c || a + c < b || b + c < a ) {
+      throw new Error ('Треугольник с такими сторонами не существует');
+    }
+  }
+
+  get perimeter() {
+    return this.a + this.b + this.c;
+  } 
+
+  get area() {
+    const p = perimeter() / 2;
+    return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+  } 
+} 
+  
+function getTriangle(a,b,c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch (error) {
+    return {
+      get perimeter() {
+        return 'Ошибка! Треугольник не существует';
+      },
+
+      get area(){
+        return 'Ошибка! Треугольник не существует';
+      }
+    }
+  }
+}
