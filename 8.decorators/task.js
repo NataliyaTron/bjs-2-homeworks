d52ede8e63997369669750c11d3a2424dbab138d
NataliyaTron <118108299+NataliyaTron@users.noreply.github.com>
2023-03-24 21:31:01 +0400

    Восьмое задание полностью готово

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,45 @@
-//Задача № 1
+//Задача №1. Усовершенствовать кеширующий декоратор
+
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(','); 
+    let objectInCache = cache.findIndex((item) => item.hash === hash); 
+    if (objectInCache != -1) { 
+      console.log('Из кэша: ' + cache[objectInCache].result); 
+      return 'Из кэша: ' + cache[objectInCache].result;
+    }
+
+    let result = func(...args); 
+    cache.push({hash, result}); // 
+    if (cache.length > 5) {
+      cache.shift()   
+    }
+    console.log('Вычисляем: '+ result);
+    return 'Вычисляем: ' + result;
+  }
+  return wrapper;
 }
 
-//Задача № 2
+//Задача № 2. Декоратор debounce с моментальным вызовом и подсчётом количества вызовов
+
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId = null;
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  function wrapper(...args) {
+    wrapper.allCount++;
+    if(timeoutId === null) {
+      func(...args);
+      wrapper.count++;
+    }
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      wrapper.count++;
+      func(...args);
+    }, delay);
+  }
+  return wrapper;
 }
